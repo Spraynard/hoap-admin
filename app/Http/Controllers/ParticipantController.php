ce9cf3bfc9c4f233c9faed8fed4f1ddf1ac7e6a1
Kyle Blanker <kyleblanker@egrtechnologies.com>
2019-11-02 16:19:57 -0400

    Cleaned up routes, removed unused templates and methods from Participant Controller

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -9,21 +9,6 @@ use Illuminate\Http\Request;
 class ParticipantController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // $participants = Participant::orderBy('created_at', 'asc')->get();
-        // // return Participant::where('firstname', 'Jane')->get();
-        // // If using DB
-        // // $participants = DB::select(SELECT * FROM participants);
-        // // $participants = Participant::orderBy('created_at', 'asc')->paginate(10);
-        // return view('participants.index')->with('participants', $participants);
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
@@ -67,51 +52,5 @@ class ParticipantController extends Controller
 
 
         return redirect('/participants')->with('success', 'Participant Created');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        // $participant = Participant::find($id);
-        // return view('participants.show')->with('participant', $participant);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
