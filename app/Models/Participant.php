2e6d1c10f12cc6362c774320eea418c5c7852d7e
Dan Drust <dandrust@gmail.com>
2019-11-02 15:05:17 -0400

    Fix community partners namespace for voyager

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+
+    public $additional_attributes = ['full_name'];
+
     public function programs()
     {
         return $this->belongsToMany(Program::class);
@@ -14,5 +17,10 @@ class Participant extends Model
     public function children()
     {
         return $this->hasMany(Children::class);
+    }
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
     }
 }
--- app/Volunteer.php
+++ app/Volunteer.php
@@ -6,11 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Volunteer extends Model
 {
+    public $additional_attributes = ['full_name'];
+
     /**
      * Get the timeEntries for the volunteer post.
      */
     public function timeEntries()
     {
         return $this->hasMany('App\TimeEntry');
+    }
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->firstName} {$this->lastName}";
     }
 }
