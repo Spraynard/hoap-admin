7e8383e33112bf512e200ea22e649a1edde0b7b9
Keenan <keenan.414@gmail.com>
2019-11-02 08:59:35 -0400

    Edited welcome page for test.F

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Code For Good: HOAP
                 </div>
 
                 <div class="links">
