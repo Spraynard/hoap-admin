62fc46b87a938f6be880fec5ee79c4fe97f2329f
Spraynard <kellan.martin@gmail.com>
2019-11-02 13:36:38 -0400

    Updating documentation on the model and such

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Donor;
 
+/**
+ * Model that encompasses donations, made by Donors, to the HOAP Administration.
+ */
 class Donation extends Model
 {
 	public function donor()
