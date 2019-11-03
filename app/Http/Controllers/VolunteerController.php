d2fd56f9a43478f3bfb4e5e5ba208af678c23632
Dan Drust <dandrust@gmail.com>
2019-11-03 10:09:39 -0500

    Fix community partners namespace for voyager

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Donor;
-use App\Volunteer;
+use App\Models\Volunteer;
 
 class VolunteerController extends Controller
 {
