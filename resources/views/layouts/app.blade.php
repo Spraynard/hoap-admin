3d91800c3e7670d4db10bf1ab30c3eea2d027e8d
Kyle Blanker <kyleblanker@egrtechnologies.com>
2019-11-03 12:19:35 -0500

    Added form styling css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 
-
         <!-- Styles -->
         <style>
             html, body {
@@ -18,38 +17,35 @@
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
-                height: 100vh;
                 margin: 0;
+                min-height: 100vh;
             }
-
+            body {
+              padding-bottom: 50px;
+              background: #f2f2f2;
+            }
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -59,30 +55,72 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
+            /* Styles for form */
             .title {
-              padding: 100px 0 50px;
+              padding: 50px 0;
             }
             .card {
-              display: flex;
-              justify-content: center;
+              min-width: 600px;
+              max-width: 800px;
+              margin: 0 auto;
+              padding: 50px;
+              box-shadow: 0 0 15px rgba(0,0,0,0.1);
+              background: #fff;
             }
             .card-body {
-              max-width: 400px;
               display: flex;
               justify-content: space-between;
             }
-            .card-body label {
+            .flex-col {
+              width: 45%;
+              display: flex;
+              flex-direction: column;
+              align-items: initial;
+              justify-content: space-evenly;
+            }
+            .card label {
               display: inline-block;
               width: auto;
               text-align: left;
             }
-            .card-body input{
-              max-width: 300px;
+            .card .full-width label {
+              width: 100% !important;
+            }
+            .card input{
+              max-width: 150px;
               display: inline-block;
+              border: none;
+              box-shadow: none;
+              border-bottom: 1px solid #aaa;
+              border-radius: 0;
+            }
+            .card input:active,
+            .card input:focus {
+              outline: 0;
+              border: 0;
+              box-shadow: none;
+              border-bottom: 1px solid #F21890;
+              background: #fff;
+            }
+            .card select {
+              border: 0;
+              border-bottom: 1px solid #aaa;
+              background: #fff;
+              -webkit-appearance: none;
+              -webkit-border-radius: 0px;
+              max-width: 150px;
+            }
+            .card textarea {
+              border: 1px solid #aaa;
+            }
+            .card textarea:active,
+            .card textarea:focus {
+              outline: 0;
+              box-shadow: none;
+              border: 1px solid #F21890;
             }
             .card-body .form-group {
               display: flex;
@@ -92,6 +130,22 @@
               max-width: unset;
               width: 100%;
             }
+            @media (max-width: 767px) {
+              .title {
+                font-size: 60px;
+              }
+              .card {
+                min-width: unset;
+                padding: 40px;
+              }
+              .card-body {
+                flex-direction: column;
+              }
+              .card .flex-col {
+                width: 100%;
+              }
+            }  
+/* End styles for form */
         </style>
     </head>
   <body>
