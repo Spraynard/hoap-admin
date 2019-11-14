4ef500aca5d2ceb3a0cf2428a2acb06388216dce
Spraynard <kellan.martin@gmail.com>
2019-11-14 17:48:37 -0500

    Removing a whitespace on app config

--- config/app.php
+++ config/app.php
@@ -237,7 +237,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
     ],
 
 ];
